d93efe00af15d5faca0e290e2624d0500769192a
Sudhir <120253668+prabha1958@users.noreply.github.com>
2026-02-13 10:47:19 +0800

    console

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Console\Commands\SendAnniversaryGreetings;
 use App\Console\Commands\SendBirthdayWishes;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('greetings:anniversary {--date=}', function () {
     $this->call(SendAnniversaryGreetings::class, [
@@ -13,5 +14,13 @@ Artisan::command('greetings:anniversary {--date=}', function () {
 
 
 Artisan::command('send:birthday-wishes', function () {
-    $this->call(SendBirthdayWishes::class,);
+    $this->call(SendBirthdayWishes::class);
 });
+
+Schedule::command('send:birthday-wishes')
+    ->dailyAt('08:00')
+    ->withoutOverlapping();
+
+Schedule::command('greetings:anniversary')
+    ->dailyAt('08:10')
+    ->withoutOverlapping();
